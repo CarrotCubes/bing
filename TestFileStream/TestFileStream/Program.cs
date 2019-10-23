1228634d354eba839c4cf2b0e8d0ada0ffcd610a
CarrotCubes <2424701399@qq.com>
2019-10-23 10:05:32 +0800

    新舔TestFileStream

---
+++ TestFileStream/TestFileStream/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestFileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string path = "TestFileStream.txt";
+            string data = "Hello,Word!";
+            byte[] byteData = new byte[100];
+            if (File.Exists(path))
+            {
+                FileStream fs = File.Create(path);
+                fs.Close();                 
+                FileStream fileWrite = File.OpenWrite(path);      
+                fileWrite.Write(Encoding.Default.GetBytes(data), 0, 11);
+                fileWrite.Close();
+                FileStream fileRead = File.OpenRead(path);
+                fileRead.Read(byteData,0,100);
+                fileRead.Close();
+                Console.WriteLine(System.Text.Encoding.Default.GetString(byteData));
+            }
+            
+        }
+    }
+}
